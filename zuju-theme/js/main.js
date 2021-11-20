b90dfd982795e4c1cab48be184ad2abd9f4c6f40
dal95 <afdallah.arafat@onyxisland.com>
2021-11-20 22:14:45 +0700

    Fix smoothscroll

--- zuju-theme/js/main.js
+++ zuju-theme/js/main.js
@@ -574,13 +574,39 @@ function setUpContinousSlide (start) {
   }).mount()
 }
 
-// var lastScrollTop = 0;
-// $(window).scroll(function(event){
-//    var st = $(this).scrollTop();
-//    if (st > lastScrollTop){
-// 	   $('.nav-sticky').removeClass('is-sticky');
-//    } else {
-// 	   $('.nav-sticky').addClass('is-sticky');
-//    }
-//    lastScrollTop = st;
-// });
+const navHeight = $('.nav-sticky').height()
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - navHeight
+        }, 300, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
